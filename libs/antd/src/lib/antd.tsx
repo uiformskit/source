61ba3349a4de2e3e93af87151ae9ae133abbf45b
Akalanka Dissanayake <connect.dissanayake@gmail.com>
2024-12-25 15:14:42 +0800

    init

---
+++ libs/antd/src/lib/antd.tsx
@@ -0,0 +1,11 @@
+import styles from './antd.module.css';
+
+export function Antd() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Antd!</h1>
+    </div>
+  );
+}
+
+export default Antd;
---
+++ libs/mui/src/lib/mui.tsx
@@ -0,0 +1,11 @@
+import styles from './mui.module.css';
+
+export function Mui() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Mui!</h1>
+    </div>
+  );
+}
+
+export default Mui;
